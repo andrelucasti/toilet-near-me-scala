d7fd404c7472465f0ec0f2bef5a90025913b991d
Andre Lucas <andrelucastic@gmail.com>
2024-06-24 18:04:31 +0100

    Add Pipeline

--- src/main/scala/infrastructure/migration/FlywayMigration.scala
+++ src/main/scala/infrastructure/migration/FlywayMigration.scala
@@ -3,12 +3,19 @@ package infrastructure.migration
 
 import org.flywaydb.core.Flyway
 
+import scala.util.Properties
+
 class FlywayMigration extends Migration {
 
   override def migrate(): Unit =
+    
+    val url = Properties.envOrElse("DATABASE_URL", "jdbc:postgresql://localhost:5432/toilet")
+    val user = Properties.envOrElse("DATABASE_USER", "toilet")
+    val password = Properties.envOrElse("DATABASE_PASSWORD", "toilet")
+    
     val flyway =
       Flyway.configure()
-        .dataSource("jdbc:postgresql://localhost:5432/toilet", "toilet", "toilet")
+        .dataSource(url, user, password)
         .locations("classpath:db/migration")
         .load()
 
